367338816d78efc16797570d7c3fe161c8e2e105
saswatsamal <samalsaswat0@gmail.com>
2021-05-06 07:56:11 +0530

    schedule added

--- js/script.js
+++ js/script.js
@@ -19,41 +19,77 @@ class scheduleEvent {
 
 var saturday = [];
 var sunday = [];
+var friday = [];
 
-// track = {main, ws} where ws is workshop
-saturday.push(new scheduleEvent("Check-In Opens", "10:00", "main"));
-saturday.push(new scheduleEvent("Hacking Begins", "12:00", "main"));
-saturday.push(new scheduleEvent("Opening Ceremony", "11:00", "main"));
-saturday.push(new scheduleEvent("Introduction to Electronics", "12:15", "13:15", "ws"));
-saturday.push(new scheduleEvent("LUNCH", "13:00", "main"));
-saturday.push(new scheduleEvent("Coding Challenge by BlackRock", "13:30", "14:30", "ws"));
-saturday.push(new scheduleEvent("Entreprenuership workshop by Edinburgh Innovations", "15:00", "16:00", "ws"));
-saturday.push(new scheduleEvent("Workshop by Nexmo", "17:00", "18:00", "ws"));
-saturday.push(new scheduleEvent("Dinner", "19:00", "main"));
-saturday.push(new scheduleEvent("Werewolf by MLH (Social)", "20:00", "21:00", "ws"));
-saturday.push(new scheduleEvent("Sleep Drop-In Begins", "21:00", "main"));
-sunday.push(new scheduleEvent("Pizza!!", "00:00", "main"));
-sunday.push(new scheduleEvent("Hacking Ends", "12:00", "main"));
-sunday.push(new scheduleEvent("Breakfast", "08:00", "main"));
-sunday.push(new scheduleEvent("Lunch", "12:00", "main"));
-sunday.push(new scheduleEvent("Judging Commences", "13:15", "14:30", "main"));
+
+friday.push(new scheduleEvent("Hackathon KickOff", "18:00", "main"));
+friday.push(new scheduleEvent("Hacking Begins", "18:30", "main"));
+friday.push(new scheduleEvent("(W)What is Hackathon?", "20:30", "main"));
+friday.push(new scheduleEvent("(M)Chill Time", "22:30", "main"));
+
+saturday.push(new scheduleEvent("(W)Twitter Bot", "11:00", "main"));
+saturday.push(new scheduleEvent("(M)Web Development Quiz", "16:00", "main"));
+saturday.push(new scheduleEvent("(W)Deploying the website", "22:30", "main"));
+
+sunday.push(new scheduleEvent("(M)Scribble", "10:00", "main"));
+sunday.push(new scheduleEvent("Hacking Ends", "18:30", "main"));
 sunday.push(new scheduleEvent("Closing Ceremony", "15:00", "16:00", "main"));
-sunday.push(new scheduleEvent("Venue closed", "17:00", "main"));
-// sunday.push(new scheduleEvent("No workshops today", "00:00", "17:00", "ws"));
+sunday.push(new scheduleEvent("Keynote and Results", "22:30", "main"));
 
+
+friday.sort((a,b) => (a.startTime >= b.startTime) ? 1 : -1 );
 saturday.sort((a,b) => (a.startTime >= b.startTime) ? 1: -1);
 sunday.sort((a,b) => (a.startTime >= b.startTime) ? 1 : -1 );
 
 var str = '<tbody>';
-str +=  '<tr><th></th><th>Main track</th><th></th><th>Workshops</th></tr>';
+str +=  '<tr><th></th><th>Main track</th></tr>';
+friday.forEach(function(ev, index){
+  if (index ==0  || friday[index-1].startTime != ev.startTime){     
+    str += '<tr>';
+
+  str += '<th>';
+
+  str += ev.startTime 
+  if (ev.endTime){
+    str+='<br />|<br />' + ev.endTime;
+  }
+  str += '</th>';
+  str += '<td>';
+  str += ev.title + '';
+  str += '</td>';
+  
+  if (ev.track == 'main'){
+      if(index != friday.length-1 && ev.startTime == friday[index+1].startTime){
+        str += '<th>';
+        str+=friday[index+1].startTime
+        if(friday[index+1].endTime){
+        str +=  '<br />|<br />' + friday[index+1].endTime;
+      }
+    
+  str += '</th>';
+  str += '<td>' + friday[index+1].title+ '</td>';
+    }
+  else{
+  str +='<th></th>';
+ 
+  }}
+  
+  str += '</tr>';
+  
+  }
+
+});
+str += '</tbody>';
+window.document.getElementById("fridayContainer").innerHTML = str;
+
+
+
+var str = '<tbody>';
+str +=  '<tr><th></th><th>Main track</th></tr>';
 saturday.forEach(function(ev, index){
   if (index ==0  || saturday[index-1].startTime != ev.startTime){     
     str += '<tr>';
-    if (ev.track == 'ws'){
 
-        str += '<th></th>';
-        str += '<td></td>';
-    }
 
   str += '<th>';
 
@@ -79,7 +115,7 @@ saturday.forEach(function(ev, index){
     }
   else{
   str +='<th></th>';
-  str += '<td></td>';
+
   }}
   
   str += '</tr>';
@@ -91,15 +127,11 @@ str += '</tbody>';
 window.document.getElementById("saturdayContainer").innerHTML = str;
 
 var str = '<tbody>';
-str +=  '<tr><th></th><th>Main track</th><th></th><th>Workshops</th></tr>';
+str +=  '<tr><th></th><th>Main track</th></tr>';
 sunday.forEach(function(ev, index){
   if (index ==0  || (sunday[index-1].startTime != ev.startTime || sunday[index-1].track == ev.track) ){     
     str += '<tr>';
-    if (ev.track == 'ws'){
 
-        str += '<th></th>';
-        str += '<td></td>';
-    }
     
     
 
@@ -128,7 +160,7 @@ sunday.forEach(function(ev, index){
   }
   else{
   str +='<th></th>';
-  str += '<td></td>';
+
   }}
   
   str += '</tr>';
@@ -138,3 +170,4 @@ sunday.forEach(function(ev, index){
 });
 str += '</tbody>';
 window.document.getElementById("sundayContainer").innerHTML = str;
+
